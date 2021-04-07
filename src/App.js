c9db89c6867d6013d97ba6eb20f1a02f337a3b34
Rafael Santos <rafaelmartins838.rs@gmail.com>
2021-04-07 03:31:02 -0300

    Pokedex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+        Pokemon: [],
+        searchfield: ''
+    }
+  }
+
+  submitChange = (event) => {
+    event.preventDefault();
+    this.getPokemon(); 
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value.toLowerCase()})  
+  }
+
+  getPokemon = async () => {
+    const toNewPoke = [];
+    try {
+       const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.searchfield}`);
+       const data = await url.json();
+       toNewPoke.push(data);
+       this.setState({Pokemon: toNewPoke});
+     } catch (error) {
+       console.log('Error!!!', error);
+     } 
+  } 
+ 
+  renderDetails = () => {
+    if (this.state.Pokemon[0].types.length === 1) {
+      return (        
+        <div key='type' className="PokeDetail">
+          <img key='img' src={this.state.Pokemon[0].sprites.front_default} width='200' height='200' alt ={this.state.Pokemon[0].name} />
+          <h3>
+            Number: {this.state.Pokemon[0].name} <br />
+            Name: {this.state.Pokemon[0].id} <br />
+            Types: {this.state.Pokemon[0].types[0].type.name} <br />
+            Size: {this.state.Pokemon[0].height/10}m <br />
+            Weight: {this.state.Pokemon[0].weight/10}kg <br />
+          </h3>
+        </div>
+      );
+    } else {
+      return (
+        <div key='types' className="PokeDetail">
+          <div className="tile">
+            <img key='img' src={this.state.Pokemon[0].sprites.front_default} width='200' height='200' alt ={this.state.Pokemon[0].name} />
+          <h3>
+              Number: {this.state.Pokemon[0].id} <br />
+              Name: {this.state.Pokemon[0].name} <br />
+              Types: {this.state.Pokemon[0].types[0].type.name} and {this.state.Pokemon[0].types[1].type.name} <br />
+              Size: {this.state.Pokemon[0].height/10}m <br />
+              Weight: {this.state.Pokemon[0].weight/10}kg <br />
+          </h3>
+          </div>
+        </div>  
+      );
+
+    }
+  }
+
+  randomGenerator = async (event) => {
+    await this.setState({searchfield: Math.floor(Math.random()* (898-1) + 1)})
+    document.getElementById("create-course-form").reset();
+    event.preventDefault();
+    this.getPokemon(); 
+  }
+
+
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={this.randomGenerator}>
+        Random Pokemon
+      </button>
+      <form onSubmit={this.submitChange} id="create-course-form">
+        <label>
+          <input 
+            type="text"
+            onChange={this.onSearchChange}
+            placeholder="Insert Pokemon name or Number"
+          />
+        </label>
+      </form>
+          {this.state.Pokemon.map((data) => {
+            return (this.renderDetails())
+          })}
     </div>
-  );
+  
+    );
+  }
+
 }
 
 export default App;
