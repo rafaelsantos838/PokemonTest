7c329ac596e5d076fc353489e6ef82aa62230074
Rafael Santos <rafaelmartins838.rs@gmail.com>
2021-04-08 13:48:35 -0300

    some changes

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ class App extends React.Component {
           <input 
             type="text"
             onChange={this.onSearchChange}
-            placeholder="Insert Pokemon Name or Number"
+            placeholder="Pokemon Name or Number"
           />
         </label>
       </form>
